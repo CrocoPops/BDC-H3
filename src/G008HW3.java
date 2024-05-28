923e0f05c559767b6df841655ae5ff744bec5a3f
eddyfri <eddy.frighetto@studenti.unipd.it>
2024-05-28 22:00:39 +0200

    True Frequent Items Method

--- src/G008HW3.java
+++ src/G008HW3.java
@@ -1,15 +1,14 @@
 import org.apache.hadoop.util.hash.Hash;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.StorageLevels;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Array;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
@@ -21,8 +20,8 @@ public class G008HW3 {
     // public static final int THRESHOLD = 1000000;
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            throw new IllegalArgumentException("USAGE: port, threshold");
+        if (args.length != 5) {
+            throw new IllegalArgumentException("USAGE: number of items, frequency of threshold, accuracy parameter, confidence parameter, port number");
         }
         // IMPORTANT: the master must be set to "local[*]" or "local[n]" with n > 1, otherwise
         // there will be no processor running the streaming computation and your
@@ -55,18 +54,33 @@ public class G008HW3 {
         // INPUT READING
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        int portExp = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[0]);
+        float phi = Float.parseFloat(args[1]);
+        if(phi <= 0 || phi >= 1) {
+            throw new IllegalArgumentException("The phi parameter must be in the range (0, 1)");
+        }
+        System.out.println("Number of items = " + n);
+        float epsilon = Float.parseFloat(args[2]);
+        if(epsilon <= 0 || epsilon >= 1) {
+            throw new IllegalArgumentException("The epsilon parameter must be in the range (0, 1)");
+        }
+        System.out.println("Epsilon = " + epsilon);
+        float delta = Float.parseFloat(args[3]);
+        if(delta <= 0 || delta >= 1) {
+            throw new IllegalArgumentException("The delta parameter must be in the range (0, 1)");
+        }
+        System.out.println("Delta = " + delta);
+        int portExp = Integer.parseInt(args[4]);
         System.out.println("Receiving data from port = " + portExp);
-        int THRESHOLD = Integer.parseInt(args[1]);
-        System.out.println("Threshold = " + THRESHOLD);
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // DEFINING THE REQUIRED DATA STRUCTURES TO MAINTAIN THE STATE OF THE STREAM
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
         long[] streamLength = new long[1]; // Stream length (an array to be passed by reference)
-        streamLength[0]=0L;
-        HashMap<Long, Long> histogram = new HashMap<>(); // Hash Table for the distinct elements
+        streamLength[0] = 0L;
+        ArrayList<ArrayList<Tuple2<Long, Long>>> trueFrequentItems = new ArrayList<>(); // True Frequent Items
+        // HashMap<Long, Long> histogram = new HashMap<>(); // Hash Table for the distinct elements
 
         // CODE TO PROCESS AN UNBOUNDED STREAM OF DATA IN BATCHES
         sc.socketTextStream("algo.dei.unipd.it", portExp, StorageLevels.MEMORY_AND_DISK)
@@ -75,25 +89,22 @@ public class G008HW3 {
                 // that the same data might be processed multiple times in case of failure.
                 .foreachRDD((batch, time) -> {
                     // this is working on the batch at time `time`.
-                    if (streamLength[0] < THRESHOLD) {
+                    if (streamLength[0] < n) {
                         long batchSize = batch.count();
                         streamLength[0] += batchSize;
-                        // Extract the distinct items from the batch
-                        Map<Long, Long> batchItems = batch
+                        JavaPairRDD<Long, Long> batchItems = batch
                                 .mapToPair(s -> new Tuple2<>(Long.parseLong(s), 1L))
-                                .reduceByKey((i1, i2) -> 1L)
-                                .collectAsMap();
-                        // Update the streaming state
-                        for (Map.Entry<Long, Long> pair : batchItems.entrySet()) {
-                            if (!histogram.containsKey(pair.getKey())) {
-                                histogram.put(pair.getKey(), 1L);
-                            }
-                        }
+                                .reduceByKey(Long::sum);
+
+                        // True Frequent Items
+                        if(batchSize > 0)
+                            trueFrequentItems.add(new ArrayList<>(trueFrequentItems(batchItems, phi, batchSize)));
+
                         // If we wanted, here we could run some additional code on the global histogram
                         if (batchSize > 0) {
                             System.out.println("Batch size at time [" + time + "] is: " + batchSize);
                         }
-                        if (streamLength[0] >= THRESHOLD) {
+                        if (streamLength[0] >= n) {
                             stoppingSemaphore.release();
                         }
                     }
@@ -106,6 +117,18 @@ public class G008HW3 {
         stoppingSemaphore.acquire();
         System.out.println("Stopping the streaming engine");
 
+        // IMPLEMENTING THE ALGORITHMS
+        // True Frequent Items with the threshold phi
+        for(ArrayList<Tuple2<Long, Long>> tfi : trueFrequentItems) {
+            tfi.sort(Comparator.comparingLong(Tuple2::_2));
+            System.out.println("True Frequent Items = " + tfi);
+            System.out.println("Number of true frequent items = " + tfi.size());
+        }
+
+        // Reservoir Sampling
+        // TODO: implement reservoir sampling
+        // epsilon-AFI computed with Sticky Sampling
+        // TODO: implement epsilon-AFI with Sticky Sampling
         // NOTE: You will see some data being processed even after the
         // shutdown command has been issued: This is because we are asking
         // to stop "gracefully", meaning that any outstanding work
@@ -115,11 +138,30 @@ public class G008HW3 {
 
         // COMPUTE AND PRINT FINAL STATISTICS
         System.out.println("Number of items processed = " + streamLength[0]);
-        System.out.println("Number of distinct items = " + histogram.size());
+        // System.out.println("Number of distinct items = " + histogram.size());
+        /*System.out.println("Histogram = " + histogram);
         long max = 0L;
         for (Long key : histogram.keySet()) {
             if (key > max) {max = key;}
         }
-        System.out.println("Largest item = " + max);
+        System.out.println("Largest item = " + max);*/
     }
+
+    /**
+     * True Frequent Items Algorithm
+     * @param stream - stream of items
+     * @param phi - frequency threshold
+     * @param streamLength - length of the stream
+     * @return - list of true frequent items
+     */
+    public static ArrayList<Tuple2<Long, Long>> trueFrequentItems(JavaPairRDD<Long, Long> stream, float phi, long streamLength) {
+        // Round 1
+        JavaPairRDD<Long, Long> frequentItems = stream
+                .reduceByKey(Long::sum)
+                .filter(s -> s._2 >= phi * streamLength);
+        // Return the list of frequent items
+        return new ArrayList<>(frequentItems.collect());
+    }
+
 }
+
